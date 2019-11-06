7f6f28ed9020384ba61e21a8bb3d5ec43398b68a
jimmyue <jimmy_job@126.com>
2019-11-06 15:53:12 +0800

    modfy readme and photo.py

--- photo.py
+++ photo.py
@@ -11,8 +11,10 @@ class ImageRecognition(TaskSet):
 		except queue.Empty:
 			print('account data run out, test ended.')
 			exit(0)
+		#open操作不能在put_nowait里操作，要不然文件会一直打开，上传的图片大小为0
+		files={'file':(request_file,open(request_file,'rb'),'image/jpg')}
 		headers={'token':'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMDAyNCIsInNlZ21lbnRfc3RhbmRhcmRfaWQiOiIxIiwidXNlcl9uYW1lIjoiYWRtaW4iLCJvcGVuSWQiOiJ0ZXN0V2VjaGF0IiwiY3VzdF9uYW1lIjoi57O757uf566h55CG5ZGYIiwib3JnSWQiOiIxIiwiaXNzdWVyIjoid2F5cyB0dWFoIEpXVCBJc3N1ZXIgMS4wIiwiZXhwIjoxNjAzNzc4NjIzfQ.cC4J7BOTulLvAyUvMsv8pLHMhwy-3PG5Imto1eH5LMCpD1eW2CwOSwvAG8znlKGnmZN1zx8WZlElvy2_6zkY1A'}
-		with self.client.post("/test/cucaInvoice/invoice/uploadPicture",headers=headers,files=request_file,catch_response=True) as response:
+		with self.client.post("/test/cucaInvoice/invoice/uploadPicture",headers=headers,files=files,catch_response=True) as response:
 			if response.status_code == 200 and response.json()['status']['status']==0:
 				response.success()
 			else:
@@ -28,5 +30,5 @@ class WebsiteUser(HttpLocust):
 	pic_list=['001.jpg','002.jpg','003.jpg','1028_1.jpg','1028_2.jpg','1028_3.jpg','1028_4.jpg','1028_5.jpg','1028_6.jpg','1028_9.jpg','1028_10.jpg','1028_12.jpg']
 	picture_data_queue = queue.Queue()
 	for i in range(len(pic_list)):
-		request_file = {'file':(pic_list[i],open(pic_list[i],'rb'),'image/jpg')} #上传图片
+		request_file = pic_list[i]
 		picture_data_queue.put_nowait(request_file)
